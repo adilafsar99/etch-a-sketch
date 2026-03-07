509f60185cab08bd71a46ee827dcaca9ddaa2650
adilafsar99 <adilafsar99@gmail.com>
2026-03-07 19:15:17 +0500

    Create a flag to decide if the grid lines should be visible when the createGrid functin runs.

--- scripts.js
+++ scripts.js
@@ -9,6 +9,9 @@ const createGrid = function (numOfSquares = 16) {
         for (j = 1; j <= columns; j++) {
             let column = document.createElement('div');
             column.classList.add('column');
+            if (show_grid) {
+                column.classList.add('showGrid');
+            }
             row.appendChild(column);
         }
         grid.appendChild(row)
@@ -18,12 +21,20 @@ const createGrid = function (numOfSquares = 16) {
 const toggleGridLines = function (event) {
     const columns = document.querySelectorAll('.column');
     columns.forEach(column => column.classList.toggle('showGrid'));
+    if (show_grid) {
+        show_grid = false;
+    }
+    else {
+        show_grid = true;
+    }
 }
 
 const adjustGrid = function (event) {
     let numOfSquares = event.target.value;
     createGrid(numOfSquares);
 }
+
+let show_grid = false;
 
 const toggleButton = document.querySelector('#toggle-button');
 toggleButton.addEventListener('click', toggleGridLines);
