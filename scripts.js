7e12f7840c15adf747564cd94f199e31c0521e8b
adilafsar99 <adilafsar99@gmail.com>
2026-03-07 07:43:20 +0500

    Create a function createGrid() to create row and column divs equal to the input.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
-const createGrid = function(event) {
-
+const createGrid = function (numOfSquares = 16) {
+    let rows = numOfSquares;
+    let columns = numOfSquares;
+    const grid = document.querySelector('.grid');
+    for (i = 1; i <= rows; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (j = 1; j <= columns; j++) {
+            let column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+        }
+        grid.appendChild(row)
+    }
 }
+
+createGrid()
